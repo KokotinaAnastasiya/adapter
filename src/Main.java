7fa98803dbaf7e22f24accc039c43a1c26ca6077
KokotinaAnastasiya <113551474+KokotinaAnastasiya@users.noreply.github.com>
2023-04-04 20:54:47 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Ints calc = new IntsCalculator();
+
+        System.out.println(calc.mult(2, 5));
+        System.out.println(calc.sum(3, 7));
+        System.out.println(calc.pow(2,6));
+
+    }
+}
